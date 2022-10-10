2989eb56744557f2d817fad3ffb8a7c8d5e1f14d
Hubercik :) <112071381+HubertGlab@users.noreply.github.com>
2022-10-10 14:24:28 +0200

    Dodańsko zadania

---
+++ main.py
@@ -0,0 +1,50 @@
+# # a)
+#
+# lista_a = ['Michal', 'Grzegorz', 'Jacek', 'Krystyna', 'Magdalena']
+#
+#
+# def wysw_imion(lista):
+#     if len(lista) == 5:
+#         for name in lista:
+#             print(name)
+#
+#
+# wysw_imion(lista_a)
+#
+# # b)
+#
+# lista_b = [1, 2, 3, 4, 5]
+#
+#
+# def mnoz_liczb(lista):
+#     liczby2 = []
+#     if len(lista) == 5:
+#         for liczba in lista:
+#             liczba = liczba * 2
+#             liczby2.append(liczba)
+#     return liczby2
+#
+# print(mnoz_liczb(lista_b))
+#
+#
+# def mnoz_liczb2(lista):
+#     if len(lista) == 5:
+#         lista_mnoz = [i * 2 for i in lista]
+#     return lista_mnoz
+#
+#
+# print(mnoz_liczb2(lista_b))
+
+# # c)
+
+# lista_c = [*range(0, 10)]
+#
+# def parzyste(lista):
+#     for liczba in lista:
+#         if liczba % 2 == 0:
+#             print(liczba)
+#
+# parzyste(lista_c)
+
+# # d)
+
